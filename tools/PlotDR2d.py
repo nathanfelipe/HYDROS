4ae4a917d405c91cb8eb197cc9c2a493b2bf25f9
nathanfelipe <felipennathan@gmail.com>
2020-05-19 11:38:42 +0200

    hydros py2->py3 #1

--- tools/PlotDR2d.py
+++ tools/PlotDR2d.py
@@ -1,14 +1,14 @@
 from pylab import *
 from sys import argv
 
-dat=genfromtxt(argv[1])
-n=len(dat)
-sn=int(sqrt(n))
-w=dat[:,0].reshape(sn,sn)
-g=dat[:,1].reshape(sn,sn)
-DR2d=dat[:,2].reshape(sn,sn)
+dat = genfromtxt(argv[1])
+n = len(dat)
+sn = int(sqrt(n))
+w = dat[:, 0].reshape(sn, sn)
+g = dat[:, 1].reshape(sn, sn)
+DR2d = dat[:, 2].reshape(sn, sn)
 
-contourf(w,g,log10(DR2d),20,cmap='gist_heat')
+contourf(w, g, log10(DR2d), 20, cmap='gist_heat')
 xlabel('Frequency')
 ylabel('Damping rate')
 colorbar()
